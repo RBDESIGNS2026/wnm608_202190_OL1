b7b6e365ad321b01f779fbddde9f4b3bff21ae36
Rebekah Brown <Info@truemadgirlartist.com>
2026-04-22 21:37:42 -0400

    Delete aau/wnm608/m11 directory

--- aau/wnm608/m11/php/checkout.php
+++
@@ -1,9 +0,0 @@
-<?php require_once("functions.php"); ?>
-
-<?php $cart = getCart(); ?>
-
-<h1>Checkout</h1>
-
-<form method="post" action="confirmation.php">
-    <button type="submit">Place Order</button>
-</form>
